f93aa66be90840ff6dff4923e34cd477f2d9a249
maharjansumina <suminamaharjan098@gmail.com>
2020-01-07 11:43:24 +0545

    Login Activity Design Changed.

--- app/src/main/java/com/softwarica/hamrobazar/LoginActivity.java
+++ app/src/main/java/com/softwarica/hamrobazar/LoginActivity.java
@@ -3,12 +3,23 @@ package com.softwarica.hamrobazar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
     }
 }
